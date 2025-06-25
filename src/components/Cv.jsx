e1a4aa55ab123d90388a637ab7688e299a55bcaa
David <davidifme@icloud.com>
2025-06-25 15:35:17 +0200

    deployment commit

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import Contact from "./Contact";
 import Strengths from "./Strengths";
-import Languages from "./Languages";
-import ModeButtons from "./ModeButtons";    
+import Languages from "./Languages"; 
 import FullName from "./FullName";
 import Education from "./Education";
 import Work from "./Work";
+import pfpImage from '../assets/images/pfp.jpeg';
 
 export default function Cv({ editMode }) {
     return (
